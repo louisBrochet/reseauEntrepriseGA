6356ecc1fffc523186c38dbeb8102a992f41d865
Martinp-95 <m.perdaens@students.ephec.be>
2021-02-03 13:05:05 +0100

    mise en place du calendrier

--- SiteWeb/Front/public/js/booking.js
+++ SiteWeb/Front/public/js/booking.js
@@ -3,3 +3,37 @@ const myForm = document.getElementById('booking-form')
 myForm.addEventListener("submit", (e) => {
     e.preventDefault();
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth()+1
+    let day = now.getDate();
+    if(month <10){
+        month = `0${month}`;
+    }
+    if(month <10){
+        day = `0${day}`;
+    }
+    dateNow = `${year}-${month}-${day}`;
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialDate: dateNow,
+      initialView: 'timeGridWeek',
+      events: [
+        {
+          title: 'Dépistage',
+          start: '2021-02-03T08:00:00',
+          end: '2021-02-03T08:30:00'
+        },
+        {
+          title: 'Vaccin',
+          start: '2021-02-03T09:00:00',
+          end: '2021-02-03T09:30:00'
+        }
+      ]
+    });
+
+    calendar.render();
+  });
