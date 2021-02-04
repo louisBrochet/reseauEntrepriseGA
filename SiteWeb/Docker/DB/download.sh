a0d08ed42f62092ee52ab2aa183d6bede01fc7f0
Amaury <amaurycrenier@gmail.com>
2021-02-04 10:47:29 +0100

    creation.sh DB

--- SiteWeb/Docker/DB/download.sh
+++ SiteWeb/Docker/DB/download.sh
@@ -1,5 +1,5 @@
-curl -o docker-compose.yml https://raw.githubusercontent.com/louisBrochet/reseauEntrepriseGA/site_web/SiteWeb/Docker/DB/docker-compose.yml
+sudo curl -o docker-compose.yml https://raw.githubusercontent.com/louisBrochet/reseauEntrepriseGA/site_web/SiteWeb/Docker/DB/docker-compose.yml
 
-mkdir script
-curl -o script/db-init.sql https://raw.githubusercontent.com/louisBrochet/reseauEntrepriseGA/site_web/SiteWeb/Docker/DB/script/db-init.sql
-curl -o script/db-data.sql https://raw.githubusercontent.com/louisBrochet/reseauEntrepriseGA/site_web/SiteWeb/Docker/DB/script/db-data.sql
+sudo mkdir script
+sudo curl -o script/db-init.sql https://raw.githubusercontent.com/louisBrochet/reseauEntrepriseGA/site_web/SiteWeb/Docker/DB/script/db-init.sql
+sudo curl -o script/db-data.sql https://raw.githubusercontent.com/louisBrochet/reseauEntrepriseGA/site_web/SiteWeb/Docker/DB/script/db-data.sql
