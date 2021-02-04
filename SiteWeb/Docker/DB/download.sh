e048e8a344da3bfe9f7778b08ee800a402075592
Amaury <amaurycrenier@gmail.com>
2021-02-04 09:30:28 +0100

    Update download.sh

--- SiteWeb/Docker/DB/download.sh
+++ SiteWeb/Docker/DB/download.sh
@@ -2,3 +2,4 @@ curl -o docker-compose.yml https://raw.githubusercontent.com/louisBrochet/reseau
 
 mkdir script
 curl -o script/db-init.sql https://raw.githubusercontent.com/louisBrochet/reseauEntrepriseGA/site_web/SiteWeb/Docker/DB/script/db-init.sql
+curl -o script/db-data.sql https://raw.githubusercontent.com/louisBrochet/reseauEntrepriseGA/site_web/SiteWeb/Docker/DB/script/db-data.sql
